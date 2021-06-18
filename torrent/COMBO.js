f9ae92e6600229561497a9f40ff29edf6738225f
Neeraj Kumar <neerajishit@gmail.com>
2021-06-19 05:11:14 +0530

    improved response time for combo search

--- torrent/COMBO.js
+++ torrent/COMBO.js
@@ -7,72 +7,45 @@ const scrapEzTVio = require('./ezTV');
 const torrentGalaxy = require('./torrentGalaxy');
 const rarbg = require('./rarbg');
 
-
 async function combo(query, page) {
     let comboTorrent = []
-    await torrentGalaxy(query, page)
-        .then((data) => {
-            if (data !== null && data.length !== 0) {
-                comboTorrent.push(data)
-            }
+    await Promise.all([
+            torrentGalaxy(query, page),
+            scrapNyaa.nyaaSI(query, page),
+            scrapYts.yts(query, page),
+            scrapPirateBay.pirateBay(query, page),
+            scrapTorLock.torLock(query, page),
+            scrapEzTVio.ezTV(query),
+            scrap1337x.torrent1337x(query, page),
+            rarbg(query, page)
+        ])
+        .then(([tgx, nyaasi, yts, piratebay, torlock, eztv, x1337, rarbg]) => {
 
-        })
-    await torrentGalaxy(query, page)
-        .then((data) => {
-            if (data !== null && data.length !== 0) {
-                comboTorrent.push(data)
+            if (tgx !== null || tgx.length !== 0) {
+                comboTorrent.push(tgx);
             }
-
-        })
-
-    await scrapNyaa.nyaaSI(query, page)
-        .then((data) => {
-            if (data !== null && data.length !== 0) {
-                comboTorrent.push(data)
+            if (nyaasi !== null || nyaasi.length !== 0) {
+                comboTorrent.push(nyaasi);
             }
-        })
-
-    await scrapYts.yts(query, page)
-        .then((data) => {
-            if (data !== null && data.length !== 0) {
-                comboTorrent.push(data)
+            if (yts !== null || yts.length !== 0) {
+                comboTorrent.push(yts);
             }
-        })
-
-    await scrapPirateBay.pirateBay(query, page)
-        .then((data) => {
-            if (data !== null && data.length !== 0) {
-                comboTorrent.push(data)
+            if (piratebay !== null || piratebay.length !== 0) {
+                comboTorrent.push(piratebay);
+            }
+            if (torlock !== null || torlock.length !== 0) {
+                comboTorrent.push(torlock);
+            }
+            if (eztv !== null || eztv.length !== 0) {
+                comboTorrent.push(eztv);
+            }
+            if (x1337 !== null || x1337.length !== 0) {
+                comboTorrent.push(x1337);
+            }
+            if (rarbg !== null || rarbg.length !== 0) {
+                comboTorrent.push(rarbg);
             }
         })
-    await scrapTorLock.torLock(query, page)
-        .then((data) => {
-            if (data !== null && data.length !== 0) {
-                comboTorrent.push(data)
-            }
-
-        })
-    await scrapEzTVio.ezTV(query)
-        .then((data) => {
-            if (data !== null && data.length !== 0) {
-                comboTorrent.push(data)
-            }
-        })
-    await scrap1337x.torrent1337x(query, page)
-        .then((data) => {
-            if (data !== null && data.length !== 0) {
-                comboTorrent.push(data)
-            }
-        })
-
-    await rarbg(query, page)
-        .then((data) => {
-            if (data !== null && data.length !== 0) {
-                comboTorrent.push(data)
-            }
-        })
-
     return comboTorrent;
 }
-
 module.exports = combo;
