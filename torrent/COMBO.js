c0bc9e9468f5212228b351e931247a9ea605d98d
maina <mainaderiq@gmail.com>
2022-03-19 14:43:05 +0300

    Added a 10s timeout to a combo.js

--- torrent/COMBO.js
+++ torrent/COMBO.js
@@ -16,112 +16,112 @@ const torrentFunk = require('./torrentFunk');
 const torrentProject = require('./torrentProject');
 
 async function combo(query, page) {
-    let comboTorrent = []
+    let comboTorrent = [], timeout = 10000 //wait time before rejecting promised results
     await Promise.allSettled([
 
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(scrap1337x.torrent1337x(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(scrapYts.yts('query', 1)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(limeTorrent(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(torrentGalaxy(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(rarbg(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(zooqle.zooqle(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(kickAss(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(scrapTorLock.torLock(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(scrapNyaa.nyaaSI(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(bitSearch(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(scrapEzTVio.ezTV(query)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(scrapPirateBay.pirateBay(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(magnet_dl(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(torrentFunk(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(glodls(query, page)))
         ]),
         Promise.race([new Promise((_, reject) => (
             setTimeout(() => {
-                reject({code:408,message:'Timeout exceeded'})
-            },10000 ))),
+                reject({code: 408, message: 'Timeout exceeded'})
+            }, timeout))),
             new Promise((resolve, _) => resolve(torrentProject(query, page)))
         ])])
         .then((comboResult) => {
-            comboTorrent = (comboResult.filter((element)=>element.status === 'fulfilled'
-                && element.value?.length>0)).map((element) => {
+            comboTorrent = (comboResult.filter((element) => element.status === 'fulfilled'
+                && element.value?.length > 0)).map((element) => {
                 return element.value
             })
         })
-        .catch(err=>console.log(err))
+        .catch(err => console.log(err))
 
     return comboTorrent;
 }
